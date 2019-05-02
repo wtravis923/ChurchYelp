0911a2d142218753aeb4894e13d4ab86ca714a41
Matthew Patterson <mdpatterson924@gmail.com>
2019-05-02 13:39:39 -0400

    first

---
+++ ChurchYelp.Models/ChurchRatingModels/ChurchRatingCreate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChurchYelp.Data;
+
+
+namespace ChurchYelp.Models.ChurchRatingModels
+{
+    public class ChurchRatingCreate
+    {
+        public int ChurchID { get; set; }
+
+        public int CommunityInvolvementRating { get; set; }
+
+        public int FriendlyRating { get; set; }
+
+        public int FacilityRating { get; set; }
+
+        public int MusicRating { get; set; }
+
+        public int MessageRating { get; set; }
+
+        public virtual Church Church { get; set; }
+    }
+}
---
+++ ChurchYelp.Models/ChurchRatingModels/ChurchRatingDetail.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChurchYelp.Data;
+
+namespace ChurchYelp.Models.ChurchRatingModels
+{
+    public class ChurchRatingDetail
+    {
+        public int ChurchID { get; set; }
+
+        public int CommunityInvolvementRating { get; set; }
+
+        public int FriendlyRating { get; set; }
+
+        public int FacilityRating { get; set; }
+
+        public int MusicRating { get; set; }
+
+        public int MessageRating { get; set; }
+
+        public virtual Church Church { get; set; }
+    }
+}
