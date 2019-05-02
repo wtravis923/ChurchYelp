d0195ba3dfa20308225917943d69c840ef397053
Spriggs01 <sarah.spriggs@gamil.com>
2019-05-02 14:08:57 -0400

    ChurchService Update

--- ChurchYelp.Data/IdentityModels.cs
+++ ChurchYelp.Data/IdentityModels.cs
@@ -1,5 +1,9 @@
-﻿using System.Security.Claims;
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using ChurchYelp.Data;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -28,6 +32,41 @@ namespace ChurchYelp.WebApi.Models
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+        public DbSet<Church> Churches { get; set; }
+
+        public DbSet<ChurchRating> ChurchRatings { get; set; }
+
+        public DbSet<Leadership> Leaders { get; set; }
+
+        public DbSet<LeadershipRating> LeadershipRatings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Conventions
+                .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
--- ChurchYelp.Data/LeadershipRating.cs
+++ ChurchYelp.Data/LeadershipRating.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace ChurchYelp.Data
 {
   public  class LeadershipRating
     {
+        [Key]
         public int LeaderID { get; set; }
         public int SpeakingAbilityRating { get; set; }
 
