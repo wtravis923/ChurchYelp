1677728af7d5e5bb58f68881dd73909959f1de95
Spriggs01 <sarah.spriggs@gamil.com>
2019-05-06 13:16:33 -0400

    added church controller

--- ChurchYelp.Data/IdentityModels.cs
+++ ChurchYelp.Data/IdentityModels.cs
@@ -13,6 +13,13 @@ namespace ChurchYelp.Data
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+        // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+        
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager, string authenticationType)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -20,6 +27,7 @@ namespace ChurchYelp.Data
             // Add custom user claims here
             return userIdentity;
         }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
---
+++ ChurchYelp.WebApi/Controllers/ChurchController.cs
@@ -0,0 +1,58 @@
+﻿using ChurchYelp.Models;
+using ChurchYelp.Services;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChurchYelp.WebApi.Controllers
+{
+    [Authorize]
+    public class ChurchController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            ChurchService churchService = new ChurchService();
+            var churches = churchService.GetChurch();
+            return Ok(churches);
+        }
+
+        public IHttpActionResult GetChurchByID(int id)
+        {
+            ChurchService churchService = new ChurchService();
+            var church = churchService.GetChurchByID(id);
+            return Ok(church);
+        }
+
+        public IHttpActionResult Post(ChurchCreate church)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = new ChurchService();
+
+            if (!service.CreateChurch(church))
+                return InternalServerError();
+
+            return Ok();
+        }
+        public IHttpActionResult Put(ChurchEdit church)
+        {
+            return Ok();
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            var service = new ChurchService();
+
+            if (!service.DeleteChurch(id))
+                return InternalServerError();
+            return Ok();
+        }
+
+        
+    }
+
+}
