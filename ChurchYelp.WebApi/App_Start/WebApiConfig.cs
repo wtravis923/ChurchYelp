766c18b0801a47ca47ec67811c3e8fb877a54c7f
wtravis923 <willie.travis@gmail.com>
2019-05-10 10:45:51 -0400

    Added Cors

--- ChurchYelp.WebApi/App_Start/WebApiConfig.cs
+++ ChurchYelp.WebApi/App_Start/WebApiConfig.cs
@@ -17,6 +17,8 @@ namespace ChurchYelp.WebApi
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
 
+
+            config.EnableCors();
             // Web API routes
             config.MapHttpAttributeRoutes();
 
